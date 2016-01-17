d0aeb6e101800467184b8c58bc1520e19f9b8c8a
Test <user1@user1s-mac-mini.local>
2016-01-17 16:57:35 +0900

    animation 2

--- libsndfileXcode/main.c
+++ libsndfileXcode/main.c
@@ -11,23 +11,16 @@ int numframes;
 
 void display()
 {
-    static int irep = 0;
+    static int irep = 44100*0;
+    int iskip = 10;
     
     glClear(GL_COLOR_BUFFER_BIT);
-    
-    glLineWidth(1.0);
-    glColor3d(1.0, 1.0, 1.0);
-    glBegin(GL_LINE_STRIP);
-    for(int i=0;i<200;i++) {
-        glVertex2d((float)i/200.0, buffer[i+irep]);
-    }
-    glEnd();
     
     glLineWidth(5.0);
     glColor3d(1.0, 1.0, 0.0);
     glBegin(GL_LINE_STRIP);
     for(int i=0;i<200;i+=2) {
-        glVertex2d((float)i/200.0, buffer[i+irep]);
+        glVertex2d((float)i/200.0, buffer[i*iskip+irep]);
     }
     glEnd();
 
@@ -35,12 +28,13 @@ void display()
     glColor3d(0.0, 1.0, 1.0);
     glBegin(GL_LINE_STRIP);
     for(int i=1;i<200;i+=2) {
-        glVertex2d((float)i/200.0, buffer[i+irep]);
+        glVertex2d((float)i/200.0, buffer[i*iskip+irep]);
     }
     glEnd();
     glFlush();
     
-    if(irep++>BUFSIZE-200) {
+    irep += 4096;
+    if(irep > numframes-200) {
         irep = 0;
     }
 }
@@ -63,7 +57,7 @@ int main(int argc, char *argv[])
     SNDFILE *fp;
     SF_INFO sfinfo;
     
-    if( (fp = sf_open("/Users/user1/SinWave.wav", SFM_READ, &sfinfo)) == NULL) {
+    if( (fp = sf_open("/Users/user1/test.wav", SFM_READ, &sfinfo)) == NULL) {
         printf("error: file not found.\n");
         return 1;
     };
@@ -71,23 +65,15 @@ int main(int argc, char *argv[])
     printf("channels   = %d \n", sfinfo.channels);
     printf("frames     = %d \n", (int)sfinfo.frames);
 
-    buffer = malloc(sfinfo.channels*BUFSIZE*sizeof(float));
+    buffer = malloc(sfinfo.channels*(int)sfinfo.frames*sizeof(float));
 
-    int totalnumframes = 0;
-    while ((numframes = (int) sf_readf_float(fp, buffer, BUFSIZE)) > 0) {
-        for(int i=0;i<numframes;i++) {
-            printf("%f \n", buffer[i]);
-        }
-        printf("numframes = %d \n", numframes);
-        totalnumframes += numframes;
-        break;
-    }
-    printf("totalnumframes = %d \n", totalnumframes);
+    numframes = (int) sf_readf_float(fp, buffer, (int)sfinfo.frames);
+    printf("numframes  = %d \n", numframes);
     
     glutInit( &argc, argv );
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA | GLUT_DEPTH);
     glutInitWindowPosition(20, 60);
-    glutInitWindowSize(1000, 750);
+    glutInitWindowSize(1000, 600);
     glutCreateWindow("libsndfileXcode");
     glClearColor(0.5, 0.8, 0.0, 1.0);
     glutDisplayFunc(display);
