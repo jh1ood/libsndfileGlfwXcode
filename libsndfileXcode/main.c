a2c6070c180901013d4e30fe29a8a7ba43025f25
Test <user1@user1s-mac-mini.local>
2016-01-17 10:33:03 +0900

    buffer size changed

--- libsndfileXcode/main.c
+++ libsndfileXcode/main.c
@@ -11,9 +11,6 @@ int main(void)
     SNDFILE *fp;
     SF_INFO sfinfo;
     
-    float *buffer;
-    buffer = malloc(2*BUFSIZE*sizeof(float));
-    
     if( (fp = sf_open("/Users/user1/SinWave.wav", SFM_READ, &sfinfo)) == NULL) {
         printf("error: file not found.\n");
         return 1;
@@ -21,12 +18,15 @@ int main(void)
     printf("samplerate = %d \n", sfinfo.samplerate);
     printf("channels   = %d \n", sfinfo.channels);
     printf("frames     = %d \n", (int)sfinfo.frames);
-    
+
+    float *buffer;
+    buffer = malloc(sfinfo.channels*(int)sfinfo.frames*sizeof(float));
+
     int numframes = (int) sf_readf_float(fp, buffer, sfinfo.frames);
     for(int i=0;i<numframes;i++) {
         printf("%f \n", buffer[i]);
     }
-    
+    printf("numframes = %d \n", numframes);
     sf_close(fp);
     
     return 0;
