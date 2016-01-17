2739f2a027c52bf1b9a57264bdf116768a9a023d
Test <user1@user1s-mac-mini.local>
2016-01-17 09:29:10 +0900

    First commit.

--- libsndfileXcode/main.c
+++ libsndfileXcode/main.c
@@ -1,15 +1,33 @@
-//
-//  main.c
-//  libsndfileXcode
-//
-//  Created by User1 on 1/17/16.
-//  Copyright © 2016 Test. All rights reserved.
-//
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#import "sndfile.h"
+#define BUFSIZE 4096
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(void)
+
+{
+    SNDFILE *fp;
+    SF_INFO sfinfo;
+    
+    float *buffer;
+    buffer = malloc(2*BUFSIZE*sizeof(float));
+    
+    if( (fp = sf_open("/Users/user1/SinWave.wav", SFM_READ, &sfinfo)) == NULL) {
+        printf("error: file not found.\n");
+        return 1;
+    };
+    printf("samplerate = %d \n", sfinfo.samplerate);
+    printf("channels   = %d \n", sfinfo.channels);
+    printf("frames     = %d \n", (int)sfinfo.frames);
+    
+    int numframes = (int) sf_readf_float(fp, buffer, sfinfo.frames);
+    for(int i=0;i<numframes;i++) {
+        printf("%f \n", buffer[i]);
+    }
+    
+    sf_close(fp);
+    
     return 0;
 }
