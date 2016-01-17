bdcaa9e9d4ccfc455174c70a341bb5f126209546
Test <user1@user1s-mac-mini.local>
2016-01-17 15:36:15 +0900

    line draw

--- libsndfileXcode/main.c
+++ libsndfileXcode/main.c
@@ -2,10 +2,51 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <GLUT/glut.h>
 #import "sndfile.h"
 #define BUFSIZE 4096
 
-int main(void)
+float* buffer;
+int numframes;
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    glLineWidth(1.0);
+    glColor3d(1.0, 1.0, 1.0);
+    glBegin(GL_LINE_STRIP);
+    for(int i=0;i<200;i++) {
+        glVertex2d((float)i/200.0, buffer[i]);
+    }
+    glEnd();
+    
+    glLineWidth(5.0);
+    glColor3d(1.0, 1.0, 0.0);
+    glBegin(GL_LINE_STRIP);
+    for(int i=0;i<200;i+=2) {
+        glVertex2d((float)i/200.0, buffer[i]);
+    }
+    glEnd();
+
+    glLineWidth(5.0);
+    glColor3d(0.0, 1.0, 1.0);
+    glBegin(GL_LINE_STRIP);
+    for(int i=1;i<200;i+=2) {
+        glVertex2d((float)i/200.0, buffer[i]);
+    }
+    glEnd();
+    glFlush();
+}
+
+void resize(int w, int h)
+{
+    glViewport(0, 0, w, h);
+    glLoadIdentity();
+    glOrtho(0.0, 1.0, -1.1, 1.1, -1.0, 1.0);
+}
+
+int main(int argc, char *argv[])
 
 {
     SNDFILE *fp;
@@ -19,10 +60,8 @@ int main(void)
     printf("channels   = %d \n", sfinfo.channels);
     printf("frames     = %d \n", (int)sfinfo.frames);
 
-    float *buffer;
     buffer = malloc(sfinfo.channels*BUFSIZE*sizeof(float));
 
-    int numframes;
     int totalnumframes = 0;
     while ((numframes = (int) sf_readf_float(fp, buffer, BUFSIZE)) > 0) {
         for(int i=0;i<numframes;i++) {
@@ -30,8 +69,20 @@ int main(void)
         }
         printf("numframes = %d \n", numframes);
         totalnumframes += numframes;
+        break;
     }
     printf("totalnumframes = %d \n", totalnumframes);
+    
+    glutInit( &argc, argv );
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA | GLUT_DEPTH);
+    glutInitWindowPosition(20, 60);
+    glutInitWindowSize(1000, 750);
+    glutCreateWindow("libsndfileXcode");
+    glClearColor(0.5, 0.8, 0.0, 1.0);
+    glutDisplayFunc(display);
+    glutReshapeFunc(resize);
+    glutMainLoop();
+    
     sf_close(fp);
     
     return 0;
