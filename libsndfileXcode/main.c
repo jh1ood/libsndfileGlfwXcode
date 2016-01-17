63c2580192b879bd3cda86762a5d401984fb018b
Test <user1@user1s-mac-mini.local>
2016-01-17 09:23:37 +0900

    Initial Commit

---
+++ libsndfileXcode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  libsndfileXcode
+//
+//  Created by User1 on 1/17/16.
+//  Copyright © 2016 Test. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
